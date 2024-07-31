2f8dde43bd183acdb07339fd81ae62aac957a14a
Carlos Henrique <ichfs9108@gmail.com>
2024-07-30 23:07:44 -0300

    feat: Criando o POST de Users

---
+++ quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/UserResource.java
@@ -0,0 +1,17 @@
+package io.github.dougllasfps.quarkussocial.rest;
+
+import io.github.dougllasfps.quarkussocial.rest.dto.CreateUserRequest;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+@Path("/users")
+public class UserResource {
+
+    @POST
+    public Response createUser(CreateUserRequest userRequest) {
+        return Response.ok().build();
+    }
+
+}
+
