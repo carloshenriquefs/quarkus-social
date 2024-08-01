30ccb198a0b4cab886d7f61054eb594e13c83206
Carlos Henrique <ichfs9108@gmail.com>
2024-07-31 23:23:09 -0300

    feat: Recebendo e devolvendo objetos JSON

--- quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/UserResource.java
+++ quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/UserResource.java
@@ -1,15 +1,24 @@
 package io.github.dougllasfps.quarkussocial.rest;
 
 import io.github.dougllasfps.quarkussocial.rest.dto.CreateUserRequest;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @POST
+    @Transactional
     public Response createUser(CreateUserRequest userRequest) {
+        return Response.ok(userRequest).build();
+    }
+
+    @GET
+    public Response listAllUsers() {
         return Response.ok().build();
     }
 
