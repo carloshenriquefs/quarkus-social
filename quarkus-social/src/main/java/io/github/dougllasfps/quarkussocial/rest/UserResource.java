999684a2822153237580824e6c6b1cb74b65d1d8
Carlos Henrique <ichfs9108@gmail.com>
2024-08-01 15:19:18 -0300

    feat: Retornando um objeto de erro padrao

--- quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/UserResource.java
+++ quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/UserResource.java
@@ -3,6 +3,7 @@ package io.github.dougllasfps.quarkussocial.rest;
 import io.github.dougllasfps.quarkussocial.domain.model.User;
 import io.github.dougllasfps.quarkussocial.domain.repository.UserRepository;
 import io.github.dougllasfps.quarkussocial.rest.dto.CreateUserRequest;
+import io.github.dougllasfps.quarkussocial.rest.dto.ResponseError;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
@@ -35,9 +36,8 @@ public class UserResource {
         Set<ConstraintViolation<CreateUserRequest>> violations = validator.validate(userRequest);
 
         if (!violations.isEmpty()) {
-            ConstraintViolation<CreateUserRequest> erro = violations.stream().findAny().get();
-            String errorMessage = erro.getMessage();
-            return Response.status(400).entity(errorMessage).build();
+            ResponseError responseError = ResponseError.createFromValidation(violations);
+            return Response.status(400).entity(responseError).build();
         }
 
         User user = new User();
