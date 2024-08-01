e1c6b4d8e736b05f4845e19d93e3669762824df5
Carlos Henrique <ichfs9108@gmail.com>
2024-08-01 15:48:47 -0300

    refactor: Alterando os codigos de status para mais especificos

--- quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/UserResource.java
+++ quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/UserResource.java
@@ -36,8 +36,9 @@ public class UserResource {
         Set<ConstraintViolation<CreateUserRequest>> violations = validator.validate(userRequest);
 
         if (!violations.isEmpty()) {
-            ResponseError responseError = ResponseError.createFromValidation(violations);
-            return Response.status(400).entity(responseError).build();
+            return ResponseError
+                    .createFromValidation(violations)
+                    .withStatusCode(ResponseError.UNPROCESSABLE_ENTITY_STATUS);
         }
 
         User user = new User();
@@ -46,7 +47,10 @@ public class UserResource {
 
         repository.persist(user);
 
-        return Response.ok(user).build();
+        return Response
+                .status(Response.Status.CREATED.getStatusCode())
+                .entity(user)
+                .build();
     }
 
     @GET
@@ -65,7 +69,7 @@ public class UserResource {
         if (user != null) {
             user.setName(userData.getName());
             user.setAge(userData.getAge());
-            return Response.ok().build();
+            return Response.noContent().build();
         }
         return Response.status(Response.Status.NOT_FOUND).build();
     }
@@ -78,7 +82,7 @@ public class UserResource {
 
         if (user != null) {
             repository.delete(user);
-            return Response.ok().build();
+            return Response.noContent().build();
         }
 
         return Response.status(Response.Status.NOT_FOUND).build();
