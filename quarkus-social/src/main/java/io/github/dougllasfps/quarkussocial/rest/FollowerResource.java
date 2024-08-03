cc6ceb5a53f17caaf7a37b0fdf149f7325f12825
Carlos Henrique <ichfs9108@gmail.com>
2024-08-03 14:11:03 -0300

    feat: Evitando conflito de usuarios

--- quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/FollowerResource.java
+++ quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/FollowerResource.java
@@ -30,6 +30,12 @@ public class FollowerResource {
     public Response followUser(
             @PathParam("userId") Long userId, FollowerRequest request) {
 
+        if (userId.equals(request.getFollowerId())) {
+            return Response.status(Response.Status.CONFLICT)
+                    .entity("You can't follow yourself")
+                    .build();
+        }
+
         var user = userRepository.findById(userId);
 
         if (user == null) {
