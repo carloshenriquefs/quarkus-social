f4ac4a07ce9aaebd2f5ab153d531db50470a8969
Carlos Henrique <ichfs9108@gmail.com>
2024-08-03 00:33:57 -0300

    feat: Implementando o metodo de realizar postagens

---
+++ quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/domain/model/Post.java
@@ -0,0 +1,26 @@
+package io.github.dougllasfps.quarkussocial.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "post_text")
+    private String text;
+
+    @Column(name = "dateTime")
+    private LocalDateTime dateTime;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
--- quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/PostResource.java
+++ quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/PostResource.java
@@ -1,5 +1,12 @@
 package io.github.dougllasfps.quarkussocial.rest;
 
+import io.github.dougllasfps.quarkussocial.domain.model.Post;
+import io.github.dougllasfps.quarkussocial.domain.model.User;
+import io.github.dougllasfps.quarkussocial.domain.repository.PostRepository;
+import io.github.dougllasfps.quarkussocial.domain.repository.UserRepository;
+import io.github.dougllasfps.quarkussocial.rest.dto.CreatePostRequest;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -9,13 +16,42 @@ import jakarta.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class PostResource {
 
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+
+    @Inject
+    public PostResource(UserRepository userRepository, PostRepository postRepository) {
+        this.userRepository = userRepository;
+        this.postRepository = postRepository;
+    }
+
     @POST
-    public Response savePost() {
+    @Transactional
+    public Response savePost(@PathParam("userId") Long userId,
+                             CreatePostRequest request) {
+        User user = userRepository.findById(userId);
+
+        if (user == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        Post post = new Post();
+        post.setText(request.getText());
+        post.setUser(user);
+
+        postRepository.persist(post);
+
         return Response.status(Response.Status.CREATED).build();
     }
 
     @GET
-    public Response listPost() {
+    public Response listPost(@PathParam("userId") Long userId) {
+        User user = userRepository.findById(userId);
+
+        if (user == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
         return Response.ok().build();
     }
 }
