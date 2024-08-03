c0ecbd24f44bf3e711103f1e5437c2aca2111a03
Carlos Henrique <ichfs9108@gmail.com>
2024-08-03 13:43:13 -0300

    feat: Concluindo o PUT de followers

--- quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/FollowerResource.java
+++ quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/FollowerResource.java
@@ -1,12 +1,14 @@
 package io.github.dougllasfps.quarkussocial.rest;
 
+import io.github.dougllasfps.quarkussocial.domain.model.Follower;
 import io.github.dougllasfps.quarkussocial.domain.repository.FollowerRepository;
 import io.github.dougllasfps.quarkussocial.domain.repository.UserRepository;
+import io.github.dougllasfps.quarkussocial.rest.dto.FollowerRequest;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/users/{userId}/followers")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -21,5 +23,31 @@ public class FollowerResource {
                             UserRepository userRepository) {
         this.followerRepository = followerRepository;
         this.userRepository = userRepository;
+    }
+
+    @PUT
+    @Transactional
+    public Response followUser(
+            @PathParam("userId") Long userId, FollowerRequest request) {
+
+        var user = userRepository.findById(userId);
+
+        if (user == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        var follower = userRepository.findById(request.getFollowerId());
+
+        boolean follows = followerRepository.follows(follower, user);
+
+        if (!follows) {
+            var entity = new Follower();
+            entity.setUser(user);
+            entity.setFollower(follower);
+
+            followerRepository.persist(entity);
+        }
+
+        return Response.status(Response.Status.NO_CONTENT).build();
     }
 }
